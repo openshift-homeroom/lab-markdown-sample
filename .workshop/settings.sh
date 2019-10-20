b19707937f5b6f281eacc16001fa223d83faa5d1
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-20 19:28:38 +1100

    Create version 1.8.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -1,5 +1,5 @@
 WORKSHOP_NAME=lab-markdown-sample
-WORKSHOP_IMAGE=quay.io/openshifthomeroom/lab-markdown-sample:1.7
+WORKSHOP_IMAGE=quay.io/openshifthomeroom/lab-markdown-sample:1.8
 RESOURCE_BUDGET=medium
 MAX_SESSION_AGE=3600
 IDLE_TIMEOUT=300
